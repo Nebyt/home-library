aa7130d4bbf109460713e885aae8576f16b8b36b
Denis <nebytov.d@gmail.com>
2022-10-16 22:28:07 +0300

    added books

--- src/booksStorage/booksStorage.js
+++ src/booksStorage/booksStorage.js
@@ -14,10 +14,100 @@ const booksStorage = [
       firstName: "Кевин",
       lastName: "Хорсли"
     },
-    title: "Безграничная память. Запоминай быстро, помни долго.",
+    title: "Безграничная память. Запоминай быстро, помни долго",
     ISBN: "9785446119295",
     publisher: "Азбука",
     publishYear: "2021",
+  },
+  {
+    author: {
+      firstName: "Дэвид Фостер",
+      lastName: "Уоллес"
+    },
+    title: "Метла системы",
+    ISBN: "9785171133146",
+    publisher: "АСТ",
+    publishYear: "2022",
+  },
+  {
+    author: {
+      firstName: "Филип",
+      lastName: "Дик"
+    },
+    title: "Убик",
+    ISBN: "9785699856961",
+    publisher: "Э",
+    publishYear: "2017",
+  },
+  {
+    author: {
+      firstName: "Павел",
+      lastName: "Басинский"
+    },
+    title: "Подлинная история Анны Карениной",
+    ISBN: "9785171362393",
+    publisher: "АСТ",
+    publishYear: "2022",
+  },
+  {
+    author: {
+      firstName: "Лев",
+      lastName: "Толстой"
+    },
+    title: "Анна Каренина",
+    ISBN: "9785171123819",
+    publisher: "АСТ",
+    publishYear: "2021",
+  },
+  {
+    author: {
+      firstName: "Рэй",
+      lastName: "Брэдбери"
+    },
+    title: "Передай добро по кругу. Рассказы, повесть",
+    ISBN: "-",
+    publisher: "Молодая гвардия",
+    publishYear: "1982",
+  },
+  {
+    author: {
+      firstName: "Борис",
+      lastName: "Акунин"
+    },
+    title: "Не прощаюсь",
+    ISBN: "9785815915411",
+    publisher: "Захаров",
+    publishYear: "2019",
+  },
+  {
+    author: {
+      firstName: "Борис",
+      lastName: "Акунин"
+    },
+    title: "Планета Вода",
+    ISBN: "9785815916357",
+    publisher: "Захаров",
+    publishYear: "2021",
+  },
+  {
+    author: {
+      firstName: "Борис",
+      lastName: "Акунин"
+    },
+    title: "Нефритовые четки",
+    ISBN: "9785815916500",
+    publisher: "Захаров",
+    publishYear: "2022",
+  },
+  {
+    author: {
+      firstName: "Борис",
+      lastName: "Акунин"
+    },
+    title: "Статский советник",
+    ISBN: "9785815904668",
+    publisher: "Захаров",
+    publishYear: "2005",
   },
 ];
 
