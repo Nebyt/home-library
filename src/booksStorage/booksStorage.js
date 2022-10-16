ca06cc55e3f80718f31fa126f8281dc1efd2545a
Denis <nebytov.d@gmail.com>
2022-10-16 21:56:26 +0300

    added booksStorage.js

---
+++ src/booksStorage/booksStorage.js
@@ -0,0 +1,24 @@
+const booksStorage = [
+  {
+    author: {
+      firstName: "Карл",
+      lastName: "Фон Клаузевиц"
+    },
+    title: "О войне",
+    ISBN: "9785389191150",
+    publisher: "Питер",
+    publishYear: "2022",
+  },
+  {
+    author: {
+      firstName: "Кевин",
+      lastName: "Хорсли"
+    },
+    title: "Безграничная память. Запоминай быстро, помни долго.",
+    ISBN: "9785446119295",
+    publisher: "Азбука",
+    publishYear: "2021",
+  },
+];
+
+export default booksStorage;
