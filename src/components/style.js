ff6802d065c373c253bb4ad397b97643b7656241
Denis <nebytov.d@gmail.com>
2022-10-18 15:00:10 +0300

    Added style.js

---
+++ src/components/style.js
@@ -0,0 +1,47 @@
+const style = {
+  form: {
+    margin: 50,
+    padding: 10,
+    width: 310,
+    border: "1px solid black",
+    backgroundColor: "black",
+    color: "white"
+  },
+  inputGroup: {
+    marginBottom: 10
+  },
+  input: {
+    backgroundColor: "#EFEFFF",
+    marginLeft: 10
+  },
+  error: {
+    color: "red",
+    margin: 5
+  },
+  booksList: {
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: 10,
+    padding: 30,
+    margin: 50
+  },
+  bookRowItem: {
+    border: "1px solid #282c34",
+    backgroundColor: "#98aebd",
+    borderRadius: 40,
+    padding: 30,
+    margin: 10,
+  },
+  bookAuthor: {
+    padding: 5,
+  },
+  bookTitle: {
+    padding: 5,
+  },
+  bookAdditionalInfo: {
+    padding: 5,
+    color: '#505e69',
+  },
+};
+
+export default style;
