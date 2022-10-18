534b577eb08516e4fbfb29392c5162013aa8e2dc
Denis <nebytov.d@gmail.com>
2022-10-18 15:00:00 +0300

    Added filter for the search field

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './style';
 import booksStorage from '../booksStorage/booksStorage';
 import BookItem from "./BookItem";
+import SearchField from "./SearchField";
 
-const BooksList = () => (
-  <div style={style.booksList}>
-    <div>
-      <label>Книг в библиотеке: {booksStorage.length}</label>
+const BooksList = () => {
+  const [filteredList, setFilteredList] = new useState(booksStorage);
+
+  const filterBySearch = (event) => {
+    // Access input value
+    const query = event.target.value;
+    // Create copy of books storage
+    let updatedList = [...booksStorage];
+    // Include all elements which includes the search query
+    updatedList = updatedList.filter(item =>
+      item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+      || item.author.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1
+      || item.author.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
+    setFilteredList(updatedList);
+  };
+
+  return (
+    <div style={style.booksList}>
+      <div>
+        <label>Книг в библиотеке: {booksStorage.length}</label>
+        <SearchField onChange={filterBySearch}/>
+      </div>
+      {filteredList.map(book =>
+        <BookItem
+          author={book.author}
+          title={book.title}
+          ISBN={book.ISBN}
+          publisher={book.publisher}
+          publishYear={book.publishYear}
+        />)}
     </div>
-    {booksStorage.map(book =>
-      <BookItem
-        author={book.author}
-        title={book.title}
-        ISBN={book.ISBN}
-        publisher={book.publisher}
-        publishYear={book.publishYear}
-      />)}
-  </div>
-);
+  );
+};
 
 export default BooksList;
